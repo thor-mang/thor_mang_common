31809204cf01c9854d626488782e6c21286fb8ec
Christian Rose <christian.j.rose@gmx.de>
2015-05-20 18:08:13 -0400

    action server for start and stop added. Publisher for falling state added

--- thor_mang_fall_detection/src/thor_mang_fall_detection_node.cpp
+++ thor_mang_fall_detection/src/thor_mang_fall_detection_node.cpp
@@ -3,9 +3,13 @@
 #include <actionlib/client/simple_action_client.h>
 #include <vigir_humanoid_control_msgs/ChangeControlModeAction.h>
 #include "sensor_msgs/Imu.h"
+#include "std_msgs/Bool.h"
 #include "tf/tf.h"
+#include <actionlib/server/simple_action_server.h>
+#include "thor_mang_fall_detection/FallDetectionControlAction.h"
 
 typedef actionlib::SimpleActionClient<vigir_humanoid_control_msgs::ChangeControlModeAction> ChangeControlModeActionClient;
+typedef actionlib::SimpleActionServer<thor_mang_fall_detection::FallDetectionControlAction> FallDetectionControlActionServer;
 
 double rollThresholdPositive = 0.245;
 double rollThresholdNegative = -0.325;
@@ -14,6 +18,10 @@ double pitchThresholdNegative = -0.15;
 
 ChangeControlModeActionClient *action_client;
 bool falling = false;
+
+bool running = true;
+
+ros::Publisher status_pub;
 
 void modeSwitchDoneCallback(const actionlib::SimpleClientGoalState& state,  const vigir_humanoid_control_msgs::ChangeControlModeResultConstPtr& result){
     if(state == actionlib::SimpleClientGoalState::SUCCEEDED){
@@ -49,6 +57,10 @@ void onFall(){
 
 void imuCallback(const sensor_msgs::Imu::ConstPtr msg)
 {
+    if(!running || falling){
+        return;
+    }
+
     double roll, pitch, yaw;
 
     tf::Quaternion orientation;
@@ -64,6 +76,39 @@ void imuCallback(const sensor_msgs::Imu::ConstPtr msg)
 
 }
 
+void start(){
+    running = true;
+    falling = false;
+}
+
+void stop(){
+    running = false;
+    falling = false;
+}
+
+void executeFallDetectionControlAction(const thor_mang_fall_detection::FallDetectionControlGoalConstPtr& goal, FallDetectionControlActionServer* as)
+{
+    if(goal->run && ! running){
+        start();
+    }else if(!goal->run && running){
+        stop();
+    }else{
+        //ignore
+    }
+
+    as->setSucceeded();
+}
+
+void timerCallback(const ros::TimerEvent&){
+
+    std_msgs::Bool msg;
+
+    msg.data = falling;
+
+    status_pub.publish(msg);
+
+}
+
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
@@ -72,12 +117,15 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "thor_mang_fall_detection");
 
-    ros::NodeHandle pn("~");
+    ros::NodeHandle nh("~");
 
-    pn.param("rollThresholdPositive", rollThresholdPositive, 0.245);
-    pn.param("rollThresholdNegative", rollThresholdNegative, -0.325);
-    pn.param("pitchThresholdPositive", pitchThresholdPositive, 0.3);
-    pn.param("pitchThresholdNegative", pitchThresholdNegative, -0.15);
+    nh.param("rollThresholdPositive", rollThresholdPositive, 0.245);
+    nh.param("rollThresholdNegative", rollThresholdNegative, -0.325);
+    nh.param("pitchThresholdPositive", pitchThresholdPositive, 0.3);
+    nh.param("pitchThresholdNegative", pitchThresholdNegative, -0.15);
+
+    FallDetectionControlActionServer server(nh, "/thor_mang_fall_detection/command", boost::bind(&executeFallDetectionControlAction, _1, &server), false);
+    server.start();
 
     std::string control_mode_topic = "/mode_controllers/control_mode_controller/change_control_mode";
 
@@ -90,7 +138,11 @@ int main(int argc, char **argv)
     }
     ROS_INFO("Connection to Action Server completed");
 
-    ros::Subscriber imu_sub = pn.subscribe<sensor_msgs::Imu>("/thor_mang/pelvis_imu", 1, imuCallback);
+    ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("/thor_mang/pelvis_imu", 1, imuCallback);
+
+    status_pub = nh.advertise<std_msgs::Bool>("falling", 1000);
+
+    ros::Timer timer = nh.createTimer(ros::Duration(0.1), timerCallback);
 
     ros::spin();
 
